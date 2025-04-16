e72022607417e978c2f703c56e90df32b65c1a41
tnyumm <tonyn.nedsch@gmail.com>
2025-04-16 14:03:34 +1000

    Add proper ROS 2 structure for arm_driver, vision_classifier, and disassembly framework

--- battery_disassembly_framework/setup.py
+++ battery_disassembly_framework/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'battery_disassembly_framework'
@@ -7,19 +9,21 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Exactly like this:
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tnyumm',
     maintainer_email='tnyumm@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Battery Disassembly Automation Framework',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'orchestrator_node = battery_disassembly_framework.orchestrator_node:main',
         ],
     },
 )
